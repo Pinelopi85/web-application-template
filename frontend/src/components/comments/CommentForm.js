2e766653a0f5cd31d3ed61b93bab820f2a11ea15
Melany <melanycristyg@gmail.com>
2019-11-22 08:54:12 +0100

    Write a comment implemented and button design changed

--- frontend/src/components/comments/CommentForm.js
+++ frontend/src/components/comments/CommentForm.js
@@ -1,40 +1,37 @@
-   import React from "react";
+import React from "react";
 
-     function CommentForm({onSubmit}) {
-        const [body, setBody] = React.useState("");
-    
-        const handleSubmit = () => {
-            //Passed in ev.callback
-            onSubmit({body: body});
-    
-            // Clear the input field
-            setBody("");
-        };
-    
-        return (
-            <div className="comment">
-                <div className="comment-body">
-                    <h4 className="comment-field" >Write a comment</h4>
-                    <div>
-                        <div className="form-elements">
-                            <textarea 
-                                className="form-control"
-                                value={body}
-                                onChange={e => setBody(e.target.value)} />
-                        </div>
-    
-    
-                        <div className="form-elements">
-                            <button 
-                                className="btn btn-primary" 
-                                onClick={handleSubmit}>
-                                Comment
+const CommentForm = ({ onSubmit }) => {
+    const [body, setBody] = React.useState("");
+    const handleSubmit = () => {
+        //Passed in ev.callback
+        onSubmit({ body: body });
+
+        // Clear the input field
+        setBody("");
+    };
+
+    return (
+        <div className="comment">
+            <div className="comment-body">
+                <h5 className="comment-field" >Write a comment</h5>
+                <div>
+                    <div className="form-group">
+                        <textarea
+                            className="form-control"
+                            value={body}
+                            onChange={e => setBody(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <button
+                            className="btn btn-primary"
+                            onClick={handleSubmit}>
+                            Comment
                             </button>
-                        </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-    
-    export default CommentForm;
+        </div>
+    );
+}
+
+export default CommentForm;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
